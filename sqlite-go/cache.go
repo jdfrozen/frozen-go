1e36de198595dad98b21fc3a253749ff00c8f061
jdfrozen <616769416@qq.com>
2020-09-08 10:16:34 +0800

    添加输入输出

--- sqlite-go/cache.go
+++ sqlite-go/cache.go
@@ -9,8 +9,13 @@ func insert(row Row) {
 	if indexRow >= 116 {
 		panic("保存超过最大行数")
 	}
-	pager.rows[pager.rowNum] = row
-	pager.rowNum++
+	//为空直接插入
+	if pager.rowNum == 0 {
+		pager.rows[pager.rowNum] = row
+		pager.rowNum++
+	}
+	//寻找插入的位置（二分查找）
+
 }
 
 func selectAll() {
@@ -18,4 +23,26 @@ func selectAll() {
 	for ; i < pager.rowNum; i++ {
 		fmt.Println(pager.rows[i])
 	}
+}
+
+//折半查找
+func getIndex(rows []Row, id uint16) int {
+	var startIndex = 0
+	var endIndex = len(rows) - 1
+	startRow := rows[startIndex]
+	endRow := rows[endIndex]
+	var index int
+	for startRow.id >= endRow.id {
+		index = (startIndex + endIndex + 1) / 2
+		v := rows[index]
+		if v.id == id {
+			break
+		}
+		if v.id > id {
+			endIndex = index
+		} else {
+			startIndex = index
+		}
+	}
+	return index
 }
