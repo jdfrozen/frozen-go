d720ddfcca3050134595ab861a5a788be0c91a72
jdfrozen <616769416@qq.com>
2020-09-09 10:34:40 +0800

    定义表

--- sqlite-go/table.go
+++ sqlite-go/table.go
@@ -1,6 +1,32 @@
 package main
 
-type table struct {
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+type Table struct {
 	rootPageIndex uint64
-	rowNum        uint16
+	rowNum        uint64
+}
+
+func readTable() Table {
+	onePage := readPagerOne(1)
+	var rootPageIndexb = make([]byte, 8)
+	var rowNumb = make([]byte, 8)
+	var indexbLen = len(rootPageIndexb)
+	for i, _ := range rootPageIndexb {
+		rootPageIndexb[i] = onePage[i]
+	}
+	for i, _ := range rowNumb {
+		rowNumb[i] = onePage[i+indexbLen]
+	}
+	buf := bytes.NewBuffer(rootPageIndexb)
+	var rootPageIndex uint64
+	binary.Read(buf, binary.BigEndian, &rootPageIndex)
+	agebuf := bytes.NewBuffer(rowNumb)
+	var rowNum uint64
+	binary.Read(agebuf, binary.BigEndian, &rowNum)
+	table := Table{rootPageIndex, rowNum}
+	return table
 }
